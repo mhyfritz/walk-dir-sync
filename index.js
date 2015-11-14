9e81695d8ceb6c05e8e99fd2a209e68bd155a68d
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2015-11-14 12:39:00 +0100

    Initial version.

---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function accept(absPath, stats, lstats) {
+  return true;
+}
+
+function* walk(dir, filter=accept, followLinks=false) {
+  let ret = {
+    path: {
+      rel: dir,
+      abs: path.resolve(dir)
+    },
+    directories: [],
+    files: []
+  };
+
+  let followDirs = [];
+  for (const file of fs.readdirSync(dir)) {
+    const absFilePath = path.resolve(dir, file);
+    const fileStats = fs.statSync(absFilePath);
+    const linkStats = fs.lstatSync(absFilePath);
+
+    if (!filter(absFilePath, fileStats, linkStats)) {
+      continue;
+    }
+
+    if (fileStats.isDirectory()) {
+      ret.directories.push(file)
+      if (!linkStats.isSymbolicLink() || followLinks) {
+        followDirs.push(path.join(dir, file));
+      }
+    } else {
+      ret.files.push(file);
+    }
+  }
+  yield ret;
+
+  for (const followDir of followDirs) {
+    yield* walk(followDir, filter, followLinks)
+  }
+}
+
+export default walk;
